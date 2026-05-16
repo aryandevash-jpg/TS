196033869b92c7cd9a5214eb9404a550b00286b7
aryan-jr-07 <aryanjr2010@gmail.com>
2026-05-16 16:37:48 +0530

    Fix Vite config plugin imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { tanstackRouter } from "@tanstack/router-plugin";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    tanstackRouter(),
+    TanStackRouterVite(),
+    tanstackStart(),
     react(),
     tailwindcss(),
     tsconfigPaths(),
